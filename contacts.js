9dfdedc4ab0539ed53a9cdd7558ff3229860042a
Umarov Mehrboz <umarovmehrboz@email.com>
2022-04-27 12:18:40 +0500

    reshil zadanie 1

---
+++ contacts.js
@@ -0,0 +1,30 @@
+
+let user = {
+    name: "Ahror",
+    nomer: 934108777,
+    adress: "asht",
+    El_pochta: "ahror@gmail.com      "
+}
+for (key in user) {
+    console.log(key)
+    console.log(user[key])
+}
+
+function myFunction() {
+    var x = document.createElement("BUTTON");
+    var t = document.createTextNode("Click me");
+    x.appendChild(t);
+    document.body.appendChild(x);
+  }
+
+
+let user1 = {
+    name: "Azam",
+    nomer: 924238790,
+    adress: "koniboom",
+    El_pochta: "azamjon@gmail.com"
+}
+for (key in user1) {
+    console.log(key)
+    console.log(user1[key])
+}
